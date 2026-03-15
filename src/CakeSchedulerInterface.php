be7b21cbfa32045b2e4bbc84c792ff3a4151a8db
Kevin Pfeifer <kevin.pfeifer@sunlime.at>
2026-03-15 14:48:20 +0100

    re-add psalm v7

--- src/CakeSchedulerInterface.php
+++ src/CakeSchedulerInterface.php
@@ -5,11 +5,15 @@ namespace CakeScheduler;
 
 use CakeScheduler\Scheduler\Scheduler;
 
+/**
+ * @psalm-suppress MissingInterfaceImmutableAnnotation
+ */
 interface CakeSchedulerInterface
 {
     /**
      * @param \CakeScheduler\Scheduler\Scheduler $scheduler The scheduler instance
      * @return void
+     * @psalm-suppress MissingAbstractPureAnnotation
      */
     public function schedule(Scheduler &$scheduler): void;
 }

f5972df8f93ac465ebfbf014e9c6c0015862c431
Kevin Pfeifer <kevin.pfeifer@sunlime.at>
2026-06-28 10:05:54 +0200

    adjust stan

--- src/CakeSchedulerInterface.php
+++ src/CakeSchedulerInterface.php
@@ -13,7 +13,6 @@ interface CakeSchedulerInterface
     /**
      * @param \CakeScheduler\Scheduler\Scheduler $scheduler The scheduler instance
      * @return void
-     * @psalm-suppress MissingAbstractPureAnnotation
      */
     public function schedule(Scheduler &$scheduler): void;
 }
